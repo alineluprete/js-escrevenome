a38ec7df218fd8ae3de5f75e30f3820f5b5da463
alineluprete <138119229+alineluprete@users.noreply.github.com>
2023-08-02 11:30:20 -0300

    adiconando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw(){
+  background(0);
+ circle(xBolinha,yBolinha,diametro);
+}
